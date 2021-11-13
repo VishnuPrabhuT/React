fb6257ee14b640e0c104ed5e5585b75d5d681f1f
Vishnu <vprabh@pdisoftware.com>
2021-11-12 19:18:39 -0600

    done

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,15 +1,25 @@
-import React, {Component} from 'react';
-import {Grid, Cell, List, ListItem, ListItemContent} from 'react-mdl';
-class Contact extends Component{
-    render(){
-        return(
+import React, { Component } from "react";
+import { Grid, Cell, List, ListItem, ListItemContent } from "react-mdl";
+class Contact extends Component {
+    render() {
+        return (
             <div className="contact-body">
                 <Grid className="contact-grid">
                     <Cell col={6}>
                         <h2>Vishnu Thiruganasambandam</h2>
-                        <img className="contact-avatar-image" style={{height:'300px'}} src="./images/contact-avatar.png" alt="contact-avatar"/>
+                        <img
+                            className="contact-avatar-image"
+                            style={{ height: "300px" }}
+                            src="/images/contact-avatar.png"
+                            alt="contact-avatar"
+                        />
                         <p>
-                            Hello! I'm a Software Developer by passion and profession which led me to a Bachelors and Masters Degree in Computer Science. Looking for full-time opportunities so, please take a look at my resume. If you find it interesting contact me with any of the details provided here. Grateful for your time!
+                            Hello! I'm a Software Developer by passion and
+                            profession which led me to a Bachelors and Masters
+                            Degree in Computer Science. Looking for full-time
+                            opportunities so, please take a look at my resume.
+                            If you find it interesting contact me with any of
+                            the details provided here. Grateful for your time!
                         </p>
                     </Cell>
                     <Cell col={6}>
@@ -18,20 +28,47 @@ class Contact extends Component{
                         <div className="contact-details">
                             <List>
                                 <ListItem>
-                                    <ListItemContent style={{textAlign: 'center', fontSize: '43px', fontFamily: 'Chewy'}}>
-                                        <i className="fa fa-phone-square" aria-hidden="true" />
+                                    <ListItemContent
+                                        style={{
+                                            textAlign: "center",
+                                            fontSize: "43px",
+                                            fontFamily: "Chewy",
+                                        }}
+                                    >
+                                        <i
+                                            className="fa fa-phone-square"
+                                            aria-hidden="true"
+                                        />
                                         (864) 624-7170
                                     </ListItemContent>
                                 </ListItem>
                                 <ListItem>
-                                    <ListItemContent style={{textAlign: 'center', fontSize: '43px', fontFamily: 'Chewy'}}>
-                                        <i className="fa fa-envelope" aria-hidden="true" />
+                                    <ListItemContent
+                                        style={{
+                                            textAlign: "center",
+                                            fontSize: "43px",
+                                            fontFamily: "Chewy",
+                                        }}
+                                    >
+                                        <i
+                                            className="fa fa-envelope"
+                                            aria-hidden="true"
+                                        />
                                         vthirug@clemson.edu
                                     </ListItemContent>
                                 </ListItem>
                                 <ListItem>
-                                    <ListItemContent style={{textAlign: 'center', fontSize: '43px', fontFamily: 'Chewy'}}>
-                                        <i className="fab fa-skype" aria-hidden="true" />
+                                    <ListItemContent
+                                        style={{
+                                            textAlign: "center",
+                                            fontSize: "43px",
+                                            fontFamily: "Chewy",
+                                        }}
+                                    >
+                                        <i
+                                            className="fab fa-skype"
+                                            aria-hidden="true"
+                                        />
                                         vishnussj
                                     </ListItemContent>
                                 </ListItem>
@@ -40,7 +77,7 @@ class Contact extends Component{
                     </Cell>
                 </Grid>
             </div>
-        )
+        );
     }
 }
 
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,94 +1,230 @@
-import React, {Component} from 'react';
-import {Tabs, Tab, Grid, Cell, Card, CardTitle, CardActions, CardMenu, Button, IconButton} from 'react-mdl';
-import { CardText } from 'react-mdl/lib/Card';
+import React, { Component } from "react";
+import {
+    Tabs,
+    Tab,
+    Grid,
+    Cell,
+    Card,
+    CardTitle,
+    CardActions,
+    CardMenu,
+    Button,
+    IconButton,
+} from "react-mdl";
+import { CardText } from "react-mdl/lib/Card";
 
-class Projects extends Component{
-    constructor(props){
-        super(props)
+class Projects extends Component {
+    constructor(props) {
+        super(props);
         this.state = {
-            activeTab : 0
-        }
+            activeTab: 0,
+        };
     }
-     
-    toggleTabs(activeTab){
-        var tabContent = [<div className="projects-grid">
-                            {/* TAB 1 */}
-                            <Card shadow={5} style={{minWidth: '460', margin: 'auto'}}>
-                            <CardTitle style={{color: '#fff', height: '176px', background: 'url(./images/unity-card.jpg) center / cover'}}>Unity Game Engine</CardTitle>
-                            <CardText>Successfully developed a 3D game in Unity game engine with scripts coded in C#.</CardText>
-                            <CardActions border>
-                                <a href="https://github.com/VishnuPrabhuT/RC-Game" rel="noopener noreferrer" target="_blank"><Button colored>Github</Button></a>
-                                <a href="https://github.com/VishnuPrabhuT/RC-Game/archive/master.zip" rel="noopener noreferrer" target="_blank"><Button colored>Download</Button></a>
-                                <a href="https://people.cs.clemson.edu/~vmhatre/Final/" rel="noopener noreferrer" target="_blank"><Button colored>Live Demo</Button></a>
-                            </CardActions>
-                            <CardMenu style={{color: '#fff'}}>
-                                <IconButton name='share' />
-                            </CardMenu>
-                          </Card> 
-                          <Card shadow={5} style={{minWidth: '460', margin: 'auto'}}>
-                            <CardTitle style={{color: '#fff', height: '176px', background: 'url(./images/C++-logo.png) center / cover'}}>2D Game with SDL and C++</CardTitle>
-                            <CardText>Learned SDL 2.0 libraries in C++ with robust design patterns to create a 2D game in Linux.</CardText>
-                            <CardActions border>
-                                <a href="https://github.com/VishnuPrabhuT/2D-Game-Engine" rel="noopener noreferrer" target="_blank"><Button colored>Github</Button></a>
-                                <a href="https://github.com/VishnuPrabhuT/2D-Game-Engine/archive/master.zip" rel="noopener noreferrer" target="_blank"><Button colored>Download</Button></a>
-                                <a href="https://drive.google.com/open?id=1bxtH5-jAlwhfprvb5Qkh3G-7Jsefi6x3" rel="noopener noreferrer" target="_blank"><Button colored>Demo</Button></a>
-                            </CardActions>
-                            <CardMenu style={{color: '#fff'}}>
-                                <IconButton name='share' />
-                            </CardMenu>
-                          </Card>
-                          </div>, 
-                          <div className="projects-grid">
-                          {/* TAB 2 */}
-                          <Card shadow={5} style={{minWidth: '460', margin: 'auto'}}>
-                          <CardTitle style={{color: '#fff', height: '176px', background: 'url(./images/react-logo.png) center / cover'}}>React Portfolio Project</CardTitle>
-                          <CardText>Made a Portfolio from scratch using React and React mdl for UI.</CardText>
-                          <CardActions border>
-                                <a href="https://github.com/VishnuPrabhuT/React" rel="noopener noreferrer" target="_blank"><Button colored>Github</Button></a>
-                                <a href="https://github.com/VishnuPrabhuT/React/archive/master.zip" rel="noopener noreferrer" target="_blank"><Button colored>Download</Button></a>
-                                <a href="https://vishnuprabhut.github.io/React/" rel="noopener noreferrer" target="_blank"><Button colored>Live Demo</Button></a>
-                          </CardActions>
-                          <CardMenu style={{color: '#fff'}}>
-                              <IconButton name='share' />
-                          </CardMenu>
-                        </Card>
-                        </div>, 
-                          <div className="projects-grid">
-                          {/* TAB 3 */}
-                          <Card shadow={5} style={{minWidth: '460', margin: 'auto'}}>
-                          <CardTitle style={{color: '#fff', height: '176px', background: 'url(./images/vs-logo.png) center / cover'}}>AADT Windows Application</CardTitle>
-                          <CardText>Developed robust Windows Application for data analysis using SVM libraries for the Department of Transportation (SC) and produces excel output files.</CardText>
-                          <CardActions border>
-                              <Button colored>Github</Button>
-                              <Button colored>Download</Button>
-                          </CardActions>
-                          <CardMenu style={{color: '#fff'}}>
-                              <IconButton name='share' />
-                          </CardMenu>
-                        </Card>
-                        </div>, 
-                          <div className="projects-grid">
-                          {/* TAB 4 */}
-                          <Card shadow={5} style={{minWidth: '460', margin: 'auto'}}>
-                          <CardTitle style={{color: '#fff', height: '176px', background: 'url(./images/RStudio-card.jpg) center / cover'}}>Yelp Data Analysis</CardTitle>
-                          <CardText>Performed Data Analysis on Yelp dataset using EDA and supervised learning in RStudio.</CardText>
-                          <CardActions style={{textAlign:'center'}} border>
-                              <a href="https://github.com/VishnuPrabhuT/graduateStudy/raw/master/Yelp%20Poster.png" target="_blank" rel="noopener noreferrer">
-                              <Button colored>View Poster</Button>
-                              </a>
-                          </CardActions>
-                          <CardMenu style={{color: '#fff'}}>
-                              <IconButton name='share' />
-                          </CardMenu>
-                        </Card>
-                        </div>]
-        return(tabContent[activeTab])
+
+    toggleTabs(activeTab) {
+        var tabContent = [
+            <div className="projects-grid">
+                {/* TAB 1 */}
+                <Card shadow={5} style={{ minWidth: "460", margin: "auto" }}>
+                    <CardTitle
+                        style={{
+                            color: "#fff",
+                            height: "176px",
+                            background:
+                                "url(/images/unity-card.jpg) center / cover",
+                        }}
+                    >
+                        Unity Game Engine
+                    </CardTitle>
+                    <CardText>
+                        Successfully developed a 3D game in Unity game engine
+                        with scripts coded in C#.
+                    </CardText>
+                    <CardActions border>
+                        <a
+                            href="https://github.com/VishnuPrabhuT/RC-Game"
+                            rel="noopener noreferrer"
+                            target="_blank"
+                        >
+                            <Button colored>Github</Button>
+                        </a>
+                        <a
+                            href="https://github.com/VishnuPrabhuT/RC-Game/archive/master.zip"
+                            rel="noopener noreferrer"
+                            target="_blank"
+                        >
+                            <Button colored>Download</Button>
+                        </a>
+                        <a
+                            href="https://people.cs.clemson.edu/~vmhatre/Final/"
+                            rel="noopener noreferrer"
+                            target="_blank"
+                        >
+                            <Button colored>Live Demo</Button>
+                        </a>
+                    </CardActions>
+                    <CardMenu style={{ color: "#fff" }}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                </Card>
+                <Card shadow={5} style={{ minWidth: "460", margin: "auto" }}>
+                    <CardTitle
+                        style={{
+                            color: "#fff",
+                            height: "176px",
+                            background:
+                                "url(/images/C++-logo.png) center / cover",
+                        }}
+                    >
+                        2D Game with SDL and C++
+                    </CardTitle>
+                    <CardText>
+                        Learned SDL 2.0 libraries in C++ with robust design
+                        patterns to create a 2D game in Linux.
+                    </CardText>
+                    <CardActions border>
+                        <a
+                            href="https://github.com/VishnuPrabhuT/2D-Game-Engine"
+                            rel="noopener noreferrer"
+                            target="_blank"
+                        >
+                            <Button colored>Github</Button>
+                        </a>
+                        <a
+                            href="https://github.com/VishnuPrabhuT/2D-Game-Engine/archive/master.zip"
+                            rel="noopener noreferrer"
+                            target="_blank"
+                        >
+                            <Button colored>Download</Button>
+                        </a>
+                        <a
+                            href="https://drive.google.com/open?id=1bxtH5-jAlwhfprvb5Qkh3G-7Jsefi6x3"
+                            rel="noopener noreferrer"
+                            target="_blank"
+                        >
+                            <Button colored>Demo</Button>
+                        </a>
+                    </CardActions>
+                    <CardMenu style={{ color: "#fff" }}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                </Card>
+            </div>,
+            <div className="projects-grid">
+                {/* TAB 2 */}
+                <Card shadow={5} style={{ minWidth: "460", margin: "auto" }}>
+                    <CardTitle
+                        style={{
+                            color: "#fff",
+                            height: "176px",
+                            background:
+                                "url(/images/react-logo.png) center / cover",
+                        }}
+                    >
+                        React Portfolio Project
+                    </CardTitle>
+                    <CardText>
+                        Made a Portfolio from scratch using React and React mdl
+                        for UI.
+                    </CardText>
+                    <CardActions border>
+                        <a
+                            href="https://github.com/VishnuPrabhuT/React"
+                            rel="noopener noreferrer"
+                            target="_blank"
+                        >
+                            <Button colored>Github</Button>
+                        </a>
+                        <a
+                            href="https://github.com/VishnuPrabhuT/React/archive/master.zip"
+                            rel="noopener noreferrer"
+                            target="_blank"
+                        >
+                            <Button colored>Download</Button>
+                        </a>
+                        <a
+                            href="https://vishnuprabhut.github.io/React/"
+                            rel="noopener noreferrer"
+                            target="_blank"
+                        >
+                            <Button colored>Live Demo</Button>
+                        </a>
+                    </CardActions>
+                    <CardMenu style={{ color: "#fff" }}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                </Card>
+            </div>,
+            <div className="projects-grid">
+                {/* TAB 3 */}
+                <Card shadow={5} style={{ minWidth: "460", margin: "auto" }}>
+                    <CardTitle
+                        style={{
+                            color: "#fff",
+                            height: "176px",
+                            background:
+                                "url(/images/vs-logo.png) center / cover",
+                        }}
+                    >
+                        AADT Windows Application
+                    </CardTitle>
+                    <CardText>
+                        Developed robust Windows Application for data analysis
+                        using SVM libraries for the Department of Transportation
+                        (SC) and produces excel output files.
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>Github</Button>
+                        <Button colored>Download</Button>
+                    </CardActions>
+                    <CardMenu style={{ color: "#fff" }}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                </Card>
+            </div>,
+            <div className="projects-grid">
+                {/* TAB 4 */}
+                <Card shadow={5} style={{ minWidth: "460", margin: "auto" }}>
+                    <CardTitle
+                        style={{
+                            color: "#fff",
+                            height: "176px",
+                            background:
+                                "url(/images/RStudio-card.jpg) center / cover",
+                        }}
+                    >
+                        Yelp Data Analysis
+                    </CardTitle>
+                    <CardText>
+                        Performed Data Analysis on Yelp dataset using EDA and
+                        supervised learning in RStudio.
+                    </CardText>
+                    <CardActions style={{ textAlign: "center" }} border>
+                        <a
+                            href="https://github.com/VishnuPrabhuT/graduateStudy/raw/master/Yelp%20Poster.png"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <Button colored>View Poster</Button>
+                        </a>
+                    </CardActions>
+                    <CardMenu style={{ color: "#fff" }}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                </Card>
+            </div>,
+        ];
+        return tabContent[activeTab];
     }
-    
-    render(){
-        return(
+
+    render() {
+        return (
             <div className="projects-tabs">
-                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({activeTab : tabId})} ripple>
+                <Tabs
+                    activeTab={this.state.activeTab}
+                    onChange={(tabId) => this.setState({ activeTab: tabId })}
+                    ripple
+                >
                     <Tab>Game Development</Tab>
                     <Tab>Web Development</Tab>
                     <Tab>Windows Application Development</Tab>
@@ -97,15 +233,13 @@ class Projects extends Component{
                 <section>
                     <div className="projects-grid">
                         <Grid>
-                            <Cell col={12}>
-
-                            </Cell>
+                            <Cell col={12}></Cell>
                         </Grid>
                         {this.toggleTabs(this.state.activeTab)}
                     </div>
                 </section>
             </div>
-        )
+        );
     }
 }
 
